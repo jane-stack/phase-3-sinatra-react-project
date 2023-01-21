33ba9fcfa504a7f7223afd9e419daccbab3795ba
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-01-21 12:39:40 -0700

    added my routs CURD

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,38 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
+  delete "/posts/:id" do
+    posts = Post.find(params[:id])
+    posts.destroy
+    posts.to_json
+  end
+
+  patch "/posts/:id" do
+    posts = Post.find(params[:id])
+    posts.update(
+      title: params[:title],
+      date: params[:date],
+      description: params[:description]
+    )
+    posts.to_json
+  end
+
+  post "/posts" do
+    posts = Post.create(
+      title: params[:title],
+      date: params[:date],
+      description: params[:description]
+    )
+    posts.to_json
+  end
+
+  get "/posts" do
+    posts = Post.all
+    posts.to_json
+  end
+
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    "hello"
   end
 
 end
