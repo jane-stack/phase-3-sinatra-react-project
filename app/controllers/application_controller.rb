c4b41e3a6a89571b6f67864892a7b3a1a33c9ecd
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-01-22 10:49:13 -0700

    redoing the applicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,35 +2,6 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  delete "/posts/:id" do
-    posts = Post.find(params[:id])
-    posts.destroy
-    posts.to_json
-  end
-
-  patch "/posts/:id" do
-    posts = Post.find(params[:id])
-    posts.update(
-      title: params[:title],
-      date: params[:date],
-      description: params[:description]
-    )
-    posts.to_json
-  end
-
-  post "/posts" do
-    posts = Post.create(
-      title: params[:title],
-      date: params[:date],
-      description: params[:description]
-    )
-    posts.to_json
-  end
-
-  get "/posts" do
-    posts = Post.all
-    posts.to_json
-  end
 
   get "/" do
     "hello"
